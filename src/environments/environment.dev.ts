852a27c2aab30c49454df40ef6da16908686d714
Carlos Daniel Bejarano preciado <bejaranopreciadocarlosdaniel@gmail.com>
2026-04-25 13:43:56 -0500

    tol=do listo

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment: Environment = {
   production: false,
   environment: 'development',
   api: {
-    baseUrl: 'http://localhost:5161/',
+    baseUrl: 'http://localhost:5161',
     apiKey: 'V4l3r4s-4p1K3y-S3cur3-2024!',
   },
 };
